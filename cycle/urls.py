5462f5e481207b3f3a86e68119c66b88d73c48ba
robinoldman <robin.c.oldman@gmail.com>
2023-05-11 10:50:14 +0000

    addability to post on to blog

--- cycle/urls.py
+++ cycle/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('cycle_site.urls'), name = 'cycle_site-urls'),
 ]
 
 
