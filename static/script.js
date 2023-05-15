a67ced93a16d87af3ad264064e852ce88268ece8
robinoldman <robin.c.oldman@gmail.com>
2023-05-15 20:13:39 +0000

    Add map with markers and driving route between two points

--- static/script.js
+++ static/script.js
@@ -1,25 +1,53 @@
 window.onload = function() {
-  // Initialize and add the map
-  let map;
+    // Initialize and add the map
+    let map;
+    let directionsService;
+    let directionsRenderer;
 
-  async function initMap() {
-      // The location of Uluru
-      const position = { lat: -25.344, lng: 131.031 };
+    async function initMap() {
+        // The locations
+        const position = { lat: 46.86297383978578, lng: 13.895879728836016 };
+        const position2 = { lat: 46.72194183472748, lng: 13.608861910802464 };
 
-      // The map, centered at Uluru
-      map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 4,
-          center: position,
-          mapId: "DEMO_MAP_ID",
-      });
+        // The map, centered at the new location
+        map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 9,
+            center: position,
+        });
 
-      // The marker, positioned at Uluru
-      const marker = new google.maps.Marker({
-          map: map,
-          position: position,
-          title: "Uluru",
-      });
-  }
+        // The marker, positioned at the new location
+        const marker1 = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: "Marker Location",
+        });
 
-  initMap();
+        // The second marker, positioned at a different location
+        const marker2 = new google.maps.Marker({
+            map: map,
+            position: position2,
+            title: "Marker Location 2",
+        });
+
+        // Initialize Directions Service and Renderer
+        directionsService = new google.maps.DirectionsService();
+        directionsRenderer = new google.maps.DirectionsRenderer();
+        directionsRenderer.setMap(map);
+
+        // Define the route request
+        const request = {
+            origin: position,
+            destination: position2,
+            travelMode: google.maps.TravelMode.BICYCLING,
+        };
+
+        // Calculate the route and display it on the map
+        directionsService.route(request, function(result, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsRenderer.setDirections(result);
+            }
+        });
+    }
+
+    initMap();
 };
