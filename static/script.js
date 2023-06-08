fbb29e87bcc550e126d5f870f147e3315aa75b0b
robinoldman <robin.c.oldman@gmail.com>
2023-06-08 15:26:05 +0000

    add javascript to make the panels react when active and add blurb to the inro of the website

--- static/script.js
+++ static/script.js
@@ -108,3 +108,18 @@ window.onload = function () {
 
 /*-------------route panels*/
 
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+
+}
